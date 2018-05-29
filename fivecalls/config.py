53c107999cef07781f3eb7b3f580694bfffed4e2
James Moore <james@panic.com>
2018-05-28 21:53:26 -0700

    move singleton to its own file

--- fivecalls/config.py
+++ fivecalls/config.py
@@ -2,15 +2,7 @@ import platform
 import subprocess
 
 from kivy.config import Config
-
-
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+from fivecalls.singleton import Singleton
 
 
 class KivyConfig(metaclass=Singleton):
