8fb8949bb3b5db5744067b733646c737a1c16a16
James Moore <james@panic.com>
2018-05-28 20:56:28 -0700

    make config a singleton

--- fivecalls/config.py
+++ fivecalls/config.py
@@ -4,7 +4,16 @@ import subprocess
 from kivy.config import Config
 
 
-class KivyConfig:
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class KivyConfig(metaclass=Singleton):
 
     def __init__(self):
         self.scale = 1.0
@@ -12,7 +21,7 @@ class KivyConfig:
         Config.set('kivy', 'exit_on_escape', 0)
         Config.set('kivy', 'desktop', 0)
 
-        Config.set('graphics', 'width',  480)
+        Config.set('graphics', 'width', 480)
         Config.set('graphics', 'height', 800)
         Config.set('graphics', 'borderless', 1)
 
@@ -26,3 +35,12 @@ class KivyConfig:
         self.padding = 20
         self.width = (480 * self.scale) - self.padding
         self.height = (800 * self.scale) - self.padding
+
+
+if __name__ == '__main__':
+    kc = KivyConfig()
+    print(f"display scale: {kc.scale}")
+
+    kc2 = KivyConfig()
+
+    assert(id(kc) == id(kc2))
