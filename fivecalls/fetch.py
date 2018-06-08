58923a90c3a4e9807f20b6bf2166216442511484
James Moore <james@panic.com>
2018-06-08 16:21:05 -0700

    fetch total call count

--- fivecalls/fetch.py
+++ fivecalls/fetch.py
@@ -21,9 +21,6 @@ def fetch() -> bool:
 
     data = response.json()
 
-    with open(JSON_PATH, 'w') as fp:
-        json.dump(data, fp)
-
     for i in data['issues']:
         for contact in i['contacts']:
             path = IMAGE_PATH + contact['id'] + '.jpg'
@@ -40,6 +37,22 @@ def fetch() -> bool:
                     if response.ok:
                         with open(path, 'wb') as f:
                             f.write(response.content)
+
+    try:
+        response = requests.get(
+                url="https://5calls.org/report/",
+                params={},
+        )
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+        return False
+
+    else:
+        count_data = response.json()
+        data['global_count'] = int(count_data['count'])
+
+    with open(JSON_PATH, 'w') as fp:
+        json.dump(data, fp)
 
     return True
 
