281627ccbc6f1ce2671f310bb1190a7950bdbd13
James Moore <hello@jmoore.me>
2018-07-20 14:55:32 -0700

    install hotline code in image

--- buildroot/hotline-external/board/hotline-rpi/post-build.sh
+++ buildroot/hotline-external/board/hotline-rpi/post-build.sh
@@ -3,6 +3,12 @@
 set -u
 set -e
 
+# Install hotline
+rsync -av --exclude ansible --exclude buildroot --exclude __pycache__ \
+--exclude .git* --exclude .idea --exclude .DS_Store \
+--delete \
+/opt/hotline/ ${TARGET_DIR}/opt/hotline/
+
 # Add a console on tty1
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
