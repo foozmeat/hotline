26fb9539994b6ee620e30317d8b644d90efcbdbe
James Moore <hello@jmoore.me>
2018-07-25 19:48:36 -0700

    fix up networking with dnsmasq

--- fivecalls/networking.py
+++ fivecalls/networking.py
@@ -11,24 +11,24 @@ def http_get_json(url, params={}):
     phone = SIM8XXManager()
     data = None
 
-    if sys.platform == 'linux':
-        phone.ppp_up()
+    # if sys.platform == 'linux':
+        # phone.ppp_up()
 
     try:
         response = requests.get(
                 url=url,
                 params=params,
         )
-    except requests.exceptions.RequestException:
-        print('HTTP Request failed')
+    except requests.exceptions.RequestException as e:
+        print(f'HTTP Request failed: {e}')
         return False
 
     else:
         if response.ok:
             data = response.json()
 
-    finally:
-        if sys.platform == 'linux':
-            phone.ppp_down()
+    # finally:
+        # if sys.platform == 'linux':
+            # phone.ppp_down()
 
         return data
